38fccd4073a1576b561f38c258cce5400fbc829d
Sam <samroux808@gmail.com>
2025-03-30 18:14:45 -0500

    Added ads meta tag

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,6 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-adsense-account" content="ca-pub-2088911413615580" />
         <title>16spaces</title>
         <link rel="stylesheet" href="/styles.css" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2088911413615580" crossorigin="anonymous"></script>
